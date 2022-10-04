753b4e38e1d6d39f1b81601136c690cd1e6eaf4a
ruitao <1439635048@qq.com>
2022-10-04 21:53:12 +0800

    new commit1

---
+++ newgit/src/main/java/com/rt/GitNew.java
@@ -0,0 +1,7 @@
+package com.rt;
+
+public class GitNew {
+    public static void main(String[] args) {
+        System.out.println("newgit");
+    }
+}
---
+++ newgit/src/test/java/com/rt/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package com.rt;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.rt.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
