e2650d86c435d306cd77cb0c664d37c2ad5de0dd
ruitao <1439635048@qq.com>
2022-10-04 19:30:20 +0800

    second commit

--- git-test/src/main/java/org/example/GitTest.java
+++ git-test/src/main/java/org/example/GitTest.java
@@ -3,5 +3,8 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("hellogithub");
+        System.out.println("hellogitgigi");
+        System.out.println("hellogitsisi");
     }
 }
