6682d9445db93a5956fa543befe9b26af2fdc6ea
Bigofficial <78535961+Bigofficial@users.noreply.github.com>
2022-10-04 20:11:38 +0800

    Delete ApartmentManagement/ApartmentManagement directory

--- ApartmentManagement/ApartmentManagement/src/main/java/com/example/apartmentmanagement/dao/SysMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.apartmentmanagement.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysMapper {
-    int selectFee();
-    int updateFee(int newFee);
-}
