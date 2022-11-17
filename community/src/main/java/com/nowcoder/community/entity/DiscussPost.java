a844a08f465ce04a29ac6bf82747b71c2f0e94c3
ruitao <1439635048@qq.com>
2022-11-17 14:45:30 +0800

    elsaticsearch finished

--- community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,16 +1,33 @@
 package com.nowcoder.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
+@Document(indexName = "discusspost", type = "doc", shards = 6, replicas = 3)
 public class DiscussPost {
+    @Id
     int id;
+    @Field(type = FieldType.Integer)
     int userId;
+    //互联网校招                    最大拆封 互联 联网 网校 校招                  聪明的不最大拆封
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     String content;
+    @Field(type = FieldType.Integer)
     int type; // 0 普通 1 置顶
+    @Field(type = FieldType.Integer)
     int status;//0 正常 1 精华 2 拉黑
+    @Field(type = FieldType.Date)
     Date createTime;
+    @Field(type = FieldType.Integer)
     int commentCount;//评论数量 不需要关联查询
+    @Field(type = FieldType.Double)
     double score;//热度排名
 
     @Override
